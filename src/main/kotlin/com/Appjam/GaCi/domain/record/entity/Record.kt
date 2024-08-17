b97ced18696b502e0ec6c59dd1c3be1eb2bf559a
snowykte0426 <snowykte0426@naver.com>
2024-08-17 23:18:44 +0900

    ✨feat: 레코드에 writer 추가
    글쓴이 기록 추가

--- src/main/kotlin/com/Appjam/GaCi/domain/record/entity/Record.kt
+++ src/main/kotlin/com/Appjam/GaCi/domain/record/entity/Record.kt
@@ -1,14 +1,16 @@
 package com.Appjam.GaCi.domain.record.entity
 
+import com.Appjam.GaCi.domain.user.entity.User
 import jakarta.persistence.*
 import jakarta.validation.constraints.NotNull
 import java.time.LocalDateTime
-
 
 @Entity
 @Table(name = "record")
 data class Record(
     @Id @GeneratedValue(strategy = GenerationType.AUTO) val id: Long? = null,
+
+    @NotNull @OneToOne @JoinColumn(name = "user_email", referencedColumnName = "email") var writer: User? = null,
 
     @Column(unique = true) @NotNull val title: String,
 
--- src/main/kotlin/com/Appjam/GaCi/domain/user/entity/User.kt
+++ src/main/kotlin/com/Appjam/GaCi/domain/user/entity/User.kt
@@ -2,6 +2,7 @@ package com.Appjam.GaCi.domain.user.entity
 
 import jakarta.persistence.*
 import jakarta.validation.constraints.NotNull
+import com.Appjam.GaCi.domain.record.entity.Record
 
 @Entity
 @Table(name = "users")
@@ -12,7 +13,11 @@ data class User(
 
     @NotNull @Enumerated(EnumType.STRING) val role: Role,
 
-    var name: String? = null, var picture: String? = null
+    var name: String? = null,
+    var picture: String? = null,
+
+    @OneToOne(mappedBy = "writer", cascade = [CascadeType.ALL], fetch = FetchType.LAZY)
+    var record: Record? = null
 ) {
     fun update(name: String?, picture: String?): User {
         return this.copy(name = name, picture = picture)
