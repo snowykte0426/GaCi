ae1ed7a0878ab228f732672cbec1e71d3d7cf812
snowykte0426 <snowykte0426@naver.com>
2024-08-17 19:26:39 +0900

    🎉feat: 프로젝트 구성
    기본 프로젝트 구성

---
+++ GaCi/src/main/kotlin/com/Appjam/GaCi/GaCiApplication.kt
@@ -0,0 +1,11 @@
+package com.Appjam.GaCi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GaCiApplication
+
+fun main(args: Array<String>) {
+	runApplication<GaCiApplication>(*args)
+}
---
+++ GaCi/src/test/kotlin/com/Appjam/GaCi/GaCiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.Appjam.GaCi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GaCiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
